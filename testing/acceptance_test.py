3075805f560ab810af478a61a2d109dccd65aeb0
Daniel Hahler <git@thequod.de>
2019-11-01 20:08:23 +0100

    we keep sys.last_* around here

--- testing/acceptance_test.py
+++ testing/acceptance_test.py
@@ -1166,6 +1166,11 @@ def test_frame_leak_on_failing_test(testdir):
             assert 0
 
         def test2():
+            import sys
+
+            del sys.last_traceback
+            del sys.last_value
+
             gc.collect()
             assert ref() is None
     """
